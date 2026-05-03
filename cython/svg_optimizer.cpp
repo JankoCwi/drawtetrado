469782aa2f4b3e94fde03c426507d4e6b7a8caa6
JankoCwi <135759152+JankoCwi@users.noreply.github.com>
2026-05-03 23:47:33 +0200

    svg_optimizer.cpp

--- cython/svg_optimizer.cpp
+++ cython/svg_optimizer.cpp
@@ -15,7 +15,8 @@ using ID = int32_t;    // Level * 4 + Index;
 using Position = int32_t; // 0/1/2/3
 
 const std::vector<std::vector<Position>> permutations = {
-    {0, 1, 2, 3}, {3, 0, 1, 2}, {2, 3, 0, 1}, {1, 2, 3, 0}};
+    {0, 1, 2, 3}, {3, 0, 1, 2}, {2, 3, 0, 1}, {1, 2, 3, 0},
+    {2, 1, 0, 3}, {3, 2, 1, 0}, {0, 3, 2, 1}, {1, 0, 3, 2}};
 
 struct Solution {
   std::vector<Position> positions;
