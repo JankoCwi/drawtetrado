9cc13479ec3626c3eb6604191b2856a13c2d4754
JankoCwi <135759152+JankoCwi@users.noreply.github.com>
2026-05-28 12:43:38 +0200

    Update svg_optimizer.cpp

--- cython/svg_optimizer.cpp
+++ cython/svg_optimizer.cpp
@@ -19,7 +19,7 @@ using Position = int32_t; // 0/1/2/3
 
 std::vector<std::vector<Position>> GetPermutations() {
     if (std::getenv("draw_5prime")) {
-        return {{2,3,0,1}};
+        return {{0,1,2,3}};
     }
     return {
         {0, 1, 2, 3}, {3, 0, 1, 2}, {2, 3, 0, 1}, {1, 2, 3, 0},
