0fd73b631493bc3d56890c661e96c076622d3300
JankoCwi <135759152+JankoCwi@users.noreply.github.com>
2026-06-07 15:24:42 +0200

    Update structure.py

--- src/drawtetrado/structure.py
+++ src/drawtetrado/structure.py
@@ -195,7 +195,7 @@ class Quadruplex:
 
         curr_pos = five_prime.position
         shift = (3 - curr_pos) % 4
-        self.Rotate(five_prime.tetrade_no, shift)
+        self.Rotate(shift)
 
 
         
