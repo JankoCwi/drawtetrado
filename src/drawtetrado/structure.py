8f8d57f85e8dec51da429e26d7a0dec07c28dabf
JankoCwi <135759152+JankoCwi@users.noreply.github.com>
2026-06-21 23:12:06 +0200

    Update structure.py

--- src/drawtetrado/structure.py
+++ src/drawtetrado/structure.py
@@ -110,6 +110,10 @@ class Nucleotide:
         self.center = Point(0, 0)
         self.connection_type = ConnType.UNKNOWN
 
+        # Loop
+        self.connection_label = 0
+        self.connection_label_type = ""
+
         # Parameters regarding connection from this nucleotide to another.
         # out - flow starting the connection
         # in - flow ending the conneciton
@@ -158,6 +162,92 @@ class Nucleotide:
 
 
 class Quadruplex:
+
+    
+
+        def AddLoopBulge(self, structure):
+
+            nucl_names = set(self.nucl_quad.keys())
+
+            for quadruplex_loops in self.loops:
+
+                for loop in quadruplex_loops:
+                    
+                    loop_nucleotides = loop.get("nucleotides", [])
+                    if len(loop_nucleotides) == 0:
+                        continue
+
+
+                    loop_start = structure.nucleotides.get(loop_nucleotides[0])
+                    loop_finish = structure.nucleotides.get(loop_nucleotides[-1])
+
+                    prev_name = ""
+                    next_name = ""
+                    prev_index = -1
+                    next_index = sys.maxsize
+
+                    for name, nucl in self.nucl_quad.items():
+                        if nucl.chain != loop_start["chain"]:
+                            continue
+
+                        if nucl.index < loop_start["index"] and nucl.index > prev_index:
+                            prev_index = nucl.index
+                            prev_name = name
+
+                        if nucl.index > loop_finish["index"] and nucl.index < next_index:
+                            next_index = nucl.index
+                            next_name = name
+
+
+                    if prev_name != "" and next_name != "":
+                        if self.nucl_quad[prev_name].connected_to == next_name:
+                            self.nucl_quad[prev_name].connection_label = len(loop_nucleotides)
+                            self.nucl_quad[prev_name].connection_label_type = "loop"
+
+
+            # BULGE
+            for tetrad_tracts in self.tracts:
+                for tract in tetrad_tracts:
+                    for i in range(len(tract) - 1):
+                        nucl_a = tract[i]
+                        nucl_b = tract[i + 1]
+
+
+
+                        nucl_a = self.nucl_quad[nucl_a]
+                        nucl_b = self.nucl_quad[nucl_b]
+
+                        if nucl_a.chain != nucl_b.chain:
+                            continue
+
+
+                        id_start = min(nucl_a.index, nucl_b.index)
+                        id_finish = max(nucl_a.index, nucl_b.index)
+                        bulge_count = 0
+
+
+                        for name, data in structure.nucleotides.items():
+                            if data["chain"] == nucl_a.chain and \
+                            data["index"] > id_start and \
+                            data["index"] < id_finish and \
+                            name not in nucl_names:
+                                bulge_count += 1
+
+                        if bulge_count == 0:
+                            continue
+
+                        if nucl_a.connected_to == nucl_b:
+                            label_nucl = nucl_a
+                        elif nucl_b.connected_to == nucl_a:
+                            label_nucl = nucl_b
+                        else:
+                            continue
+
+                        if label_nucl.connection_label == 0:
+                            label_nucl.connection_label = bulge_count
+                            label_nucl.connection_label_type = "bulge"
+
+
 
     def Rotate(self, shift):
         shift %= 4
@@ -357,8 +447,20 @@ class Quadruplex:
             single_tracts = []
             single_tracts.append(structure.tracts[quadruplex_id][tetrad_id])
             self.tracts = single_tracts
+
+            # Loop
+            single_loops = []
+            single_loops.append(structure.loops[quadruplex_id][tetrad_id])
+            self.loops = single.loops
+
+        
         else:
             self.tracts = structure.tracts[quadruplex_id]
+            self.loops = structure.loops[quadruplex_id]
+
+
+        self.AddBulgeLoopVisualLabels(structure)
+
 
     def GetNucleotidesPositions(self):
         lst = [-1] * len(self.nucl_quad)
